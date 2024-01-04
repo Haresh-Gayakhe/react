7b4cc7ff1a3edd7f05b9cfd582f69ea287deb492
Haresh Gayakhe <hareshgayakhe5421@gmail.com>
2024-01-04 22:47:28 +0530

    react router crash course

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+
+function Github() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/haresh-gayakhe")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      });
+  }, []);
+
+  return (
+    <>
+      <img src={data.avatar_url} />
+      <div>Github followers: {data.followers}</div>
+    </>
+  );
+}
+
+export default Github;
